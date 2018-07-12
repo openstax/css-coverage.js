c878103dc2375f8ee58c0c6e3669694b83ba45ba
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-07-12 16:59:36 -0500

    use puppeteer instead of phantomjs

--- test/mocha-demo/dummy-project/app.js
+++ test/mocha-demo/dummy-project/app.js
@@ -1,18 +1,18 @@
-define(['jquery'], function($) {
-  var $body = $('body');
+window.define(['jquery'], function ($) {
+  var $body = $('body')
 
   return {
-    doSomethingComplex: function() {
-      $body.append('<h3>Something Complex</h3>');
+    doSomethingComplex: function () {
+      $body.append('<h3>Something Complex</h3>')
     },
 
-    doSomethingElseComplex: function() {
-      $body.append('<blockquote>Something Else Complex</blockquote>');
+    doSomethingElseComplex: function () {
+      $body.append('<blockquote>Something Else Complex</blockquote>')
     },
 
-    unusedFunction: function(className) {
-      console.log('This line should never be run');
-    },
+    unusedFunction: function (className) {
+      console.log('This line should never be run')
+    }
 
-  };
-});
+  }
+})
